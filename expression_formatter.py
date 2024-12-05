2b0d1be3ff0c735a6a1258e2cff9fbf67cf4d101
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2024-12-05 13:59:01 +0200

    refactor(expression_formmater): cleaned the file

--- expression_formatter.py
+++ expression_formatter.py
@@ -10,20 +10,6 @@ class IFormatter(ABC):
         """
         pass
 
-    @abstractmethod
-    def check_str_expression(self, expression: str) -> (bool, str):
-        """
-        Check if the formatter can format the string. Return ``True`` if it can,
-        and ``False`` if it cannot. If the string cannot be formatted, returns a
-        reason for failure.
-        :param expression: Expression to be checked
-        :return: A tuple with a bool indicating success and a string for description
-        """
-        pass
-
 class InfixToPostfixFormatter(IFormatter):
     def format_str(self, expression: str) -> str:
-        pass
-
-    def check_str_expression(self, expression: str) -> (bool, str):
         pass
