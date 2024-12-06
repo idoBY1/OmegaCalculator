9826741c351b67ce2c8508ebe71d460be45a83f6
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2024-12-06 19:06:12 +0200

    feat: created classes for most operators

--- src/calculatorLogic/operator.py
+++ src/calculatorLogic/operator.py
@@ -1,7 +1,7 @@
+import math
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
-
 
 @dataclass
 class Operator(ABC):
@@ -24,7 +24,7 @@ class Operator(ABC):
         return self._symbol
 
 
-class UnaryOperator(ABC, Operator):
+class UnaryOperator(Operator):
     class OperandPos(Enum):
         """
         This class indicates the position of the operand relative to the
@@ -34,7 +34,7 @@ class UnaryOperator(ABC, Operator):
         BEFORE = 0
         AFTER = 1
 
-    operand_pos: OperandPos
+    _operand_pos: OperandPos
 
     @abstractmethod
     def operate(self, num: float) -> float:
@@ -44,3 +44,120 @@ class UnaryOperator(ABC, Operator):
         :return: The result of the operation as a floating point number
         """
         pass
+
+
+class BinaryOperator(Operator):
+    @abstractmethod
+    def operate(self, num1: float, num2: float) -> float:
+        """
+        Perform the operation on the numbers and return a result.
+        :param num1: The first floating point number to perform the operation with
+        :param num2: The second floating point number to perform the operation with
+        :return: The result of the operation as a floating point number
+        """
+        pass
+
+
+# The actual operators are implemented here
+class Addition(BinaryOperator):
+    def __init__(self):
+        self._symbol = '+'
+        self._priority = 1
+
+    def operate(self, num1: float, num2: float) -> float:
+        return num1 + num2
+
+
+class Subtraction(BinaryOperator):
+    def __init__(self):
+        self._symbol = '-'
+        self._priority = 1
+
+    def operate(self, num1: float, num2: float) -> float:
+        return num1 - num2
+
+
+class Multiplication(BinaryOperator):
+    def __init__(self):
+        self._symbol = '*'
+        self._priority = 2
+
+    def operate(self, num1: float, num2: float) -> float:
+        return num1 * num2
+
+
+class Division(BinaryOperator):
+    def __init__(self):
+        self._symbol = '/'
+        self._priority = 2
+
+    def operate(self, num1: float, num2: float) -> float:
+        return num1 / num2
+
+
+class Power(BinaryOperator):
+    def __init__(self):
+        self._symbol = '^'
+        self._priority = 3
+
+    def operate(self, num1: float, num2: float) -> float:
+        return num1 ** num2
+
+
+class Modulo(BinaryOperator):
+    def __init__(self):
+        self._symbol = '%'
+        self._priority = 4
+
+    def operate(self, num1: float, num2: float) -> float:
+        return num1 % num2
+
+
+class Max(BinaryOperator):
+    def __init__(self):
+        self._symbol = '$'
+        self._priority = 5
+
+    def operate(self, num1: float, num2: float) -> float:
+        return num1 if num1 > num2 else num2
+
+
+class Min(BinaryOperator):
+    def __init__(self):
+        self._symbol = '&'
+        self._priority = 5
+
+    def operate(self, num1: float, num2: float) -> float:
+        return num1 if num1 < num2 else num2
+
+
+class Average(BinaryOperator):
+    def __init__(self):
+        self._symbol = '@'
+        self._priority = 5
+
+    def operate(self, num1: float, num2: float) -> float:
+        return (num1 + num2) / 2.0
+
+
+class Negation(UnaryOperator):
+    def __init__(self):
+        self._symbol = '~'
+        self._priority = 6
+        self._operand_pos = UnaryOperator.OperandPos.AFTER
+
+    def operate(self, num: float) -> float:
+        return -num
+
+
+class Factorial(UnaryOperator):
+    def __init__(self):
+        self._symbol = '!'
+        self._priority = 6
+        self._operand_pos = UnaryOperator.OperandPos.BEFORE
+
+    def operate(self, num: float) -> float:
+        try:
+            return math.gamma(num + 1) # extension of factorial to real numbers
+        except ValueError:
+            print("Cannot compute factorial of " + str(num) + "!")
